f6c69262036af3439bdad957e457ff9955ffacbc
lexxycool <maxirexy16@yahoo.com>
2024-11-18 15:47:33 +0300

    Adding toast messages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 import Main, { mainLoader } from "./layouts/Main";
 import { logoutAction } from "./actions/logout";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const router = createBrowserRouter([
@@ -34,7 +36,7 @@ function App() {
 
   return <div className="App">
     <RouterProvider router={router} />
-
+    <ToastContainer />
   </div>
     
 }
--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,11 +1,15 @@
 import { redirect } from "react-router-dom";
 import { deleteItem } from "../helper";
+import { toast } from "react-toastify";
 
 export async function logoutAction() {
     //delete user
     deleteItem({
         key: "userName"
     });
+
+    toast.success("You have deleted your account!");
+
     //return a redirect
     return redirect("/")
 }
