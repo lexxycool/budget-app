106a81c5b986c28c5e1fc75261e41e856d874a6a
lexxycool <maxirexy16@yahoo.com>
2024-11-25 16:58:44 +0300

    committing login page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { fetchData } from '../helper'
 import { useLoaderData } from 'react-router-dom';
+import Intro from '../components/Intro';
+import { toast } from 'react-toastify';
 
+//loader
 export function dashboardLoader() {
-    const userName = fetchData("userName");
-    return { userName }
+  const userName = fetchData("userName");
+  return { userName }
+}
+
+//action
+export async function dashboardAction({request}) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
+  } catch (e) {
+    throw new Error('There was a problem creating your account.');
+  } 
 }
 
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
   return (
-    <div>
-     <h2>{userName}</h2>
-     Dashboard
-    </div>
+    <>
+      { userName ? (<p>{userName}</p>) : <Intro /> }
+      
+    </>
   )
 }
 
