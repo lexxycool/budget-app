926df35b8d315524efcdc6d1a31ce2bc22f80700
lexxycool <maxirexy16@yahoo.com>
2024-11-27 22:58:07 +0300

    modified code to use switch cases

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,26 +17,27 @@ export async function dashboardAction({request}) {
   const data = await request.formData();
   const { _action, ...values } = Object.fromEntries(data);
 
-  if (_action === 'newUser') {
-    try {
-      localStorage.setItem("userName", JSON.stringify(values.userName));
-      return toast.success(`Welcome, ${values.userName}`);
-    } catch (e) {
-      throw new Error('There was a problem creating your account.');
-    }
-  } 
-
-  if (_action === 'createBudget') {
-    try {
-      createBudget({
-        name: values.newBudget,
-        amount: values.newBudgetAmount
-      })
-      return toast.success("Budget created!")
-    } catch (e) {
-      throw new Error("There was a problem creating your budget.")
-    }
-  }
+  switch (_action) {
+    case 'newUser':
+      try {
+        localStorage.setItem("userName", JSON.stringify(values.userName));
+        return toast.success(`Welcome, ${values.userName}`);
+      } catch (e) {
+        throw new Error('There was a problem creating your account.');
+      };
+      break;
+    case 'createBudget':
+      try {
+        createBudget({
+          name: values.newBudget,
+          amount: values.newBudgetAmount
+        })
+        return toast.success("Budget created!")
+      } catch (e) {
+        throw new Error("There was a problem creating your budget.")
+      };
+      break;
+  }  
 }
 
 const Dashboard = () => {
